157ef41954223f001e29c2e69b77560743d5b755
Исаков Михаил Андреевич <maisakov@edu.hse.ru>
2024-03-26 22:20:08 +0700

    change config

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,6 @@
 import os
-# from transformers import pipeline
+from pathlib import Path
+from transformers import pipeline
 
 
 MONGO_HOST = os.getenv("MONGO_HOST")
@@ -8,7 +9,9 @@ MONGO_USERNAME = os.getenv("MONGO_USERNAME")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 MONGO_BASE = os.getenv('MONGO_BASE')
 
+RESTORE_DIR = Path('db_dump/')
+
 token = os.getenv("TOKEN")
 
-# recommendation_pipe = pipeline("text-classification", model="nebiyu29/fintunned-v2-roberta_GA", device='cpu')
-# translation_pipe = pipeline("text-classification", model="nebiyu29/fintunned-v2-roberta_GA", device='cpu')
+recommendation_pipe = pipeline("text-classification", model="nebiyu29/fintunned-v2-roberta_GA", device='cpu')
+translation_pipe = pipeline("text-classification", model="nebiyu29/fintunned-v2-roberta_GA", device='cpu')
